2d20f53907ea34f8325dca8e57ede7ba2429a63a
Arsen <arsen.hovhannisyan.1991@gmail.com>
2024-02-05 09:34:12 +0400

    Fix crashed migration files

--- blog/database/migrations/2024_02_05_021233_create_posts_table.php
+++ blog/database/migrations/2024_02_05_021233_create_posts_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('title');
+            $table->string('author');
+            $table->integer('publication_year');
         });
     }
 
--- notification/database/migrations/2024_02_05_021343_create_post_notifications_table.php
+++ notification/database/migrations/2024_02_05_021343_create_post_notifications_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('post_notifications', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->unsignedBigInteger('post_id');
+            $table->string('message');
         });
     }
 
